618ba5ccc07a6bb5ba1a88154a68263f2cebcd77
jiwon <32190250@dankook.ac.kr>
2024-02-06 10:32:21 +0900

    day02-자바 변수, 형 변환, 상수 개념

---
+++ src/day02/Ex05.java
@@ -0,0 +1,13 @@
+package day02;
+//상수 final - 모두 대문자로 작성
+public class Ex05 {
+	public static void main(String[] args) {
+		String Korea = "대한민국";
+		System.out.println(Korea);		
+		Korea = "미국";
+		System.out.println(Korea);
+		
+		final String KOREA = "대한민국";
+		System.out.println(KOREA);
+	}
+}
